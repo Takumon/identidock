6e0ad6fb0bf0f40d3015cbb02e1986ef677c2caf
takumon <inouetakumon@gmail.com>
2017-07-01 07:47:09 +0900

    dockerイメージプッシュ

--- jenkins_job_shell.sh
+++ jenkins_job_shell.sh
@@ -1,10 +1,12 @@
-#Default compose args
+# Unitテストをどの権限でも実行できるようにしておく
+chmod 777 app/*
+
+# 定数定義
 COMPOSE_ARGS=" -f jenkins.yml -p jenkins "
 
 #古いコンテナを削除
 sudo docker-compose $COMPOSE_ARGS stop
 sudo docker-compose $COMPOSE_ARGS rm --force -v
-
 
 # ビルド
 sudo docker-compose $COMPOSE_ARGS build --no-cache
@@ -14,13 +16,21 @@ sudo docker-compose $COMPOSE_ARGS up -d
 sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock
 EPR=$?
 
-
 # 結合テスト実行
 if [ $EPR -eq 0 ]; then
   IP=$(sudo docker inspect -f{{.NetworkSettings.IPAddress}} jenkins_identidock_1)
   CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
-  if [ $CODE -ne 200 ]; then
-    echo "Site returned " $CODE
+  if [ $CODE -eq 200 ]; then
+    echo "テスト成功 - タグ付けします"
+    HASH=$(git rev-parse --short HEAD)
+    sudo docker tag -f jenkins_identidock takumon/identidock:$HASH
+    sudo docker tag -f jenkins_identidock takumon/indentidock:newest
+    echo "プッシュします。"
+    sudo docker login $DOCKER_ACOUNT
+    sudo docker push takumon/identidock:$HASH
+    sudo docker push takumon/indentidock:newest
+  else
+    echo "テスト失敗　終了します。" $CODE
     EPR=1
   fi
 fi
